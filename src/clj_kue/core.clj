0a91481bb212658af143b202d172a6d80b96b2d8
Leonid Beschastny <lbeschastny@pliq.me>
2013-05-20 17:22:05 +0400

    Refactored worker.

--- src/clj_kue/core.clj
+++ src/clj_kue/core.clj
@@ -1,13 +1,11 @@
 (ns clj-kue.core
-  (:require [clj-kue.worker :as worker]))
+  (:require [clj-kue.worker :as worker])
+  (:use [clj-kue.worker :only [Worker]]))
 
 (defn process
   "Process jobs with the given type, invoking (f job)"
   ([type f]
-    (future (worker/start type f)))
+    (.start (Worker (name type) f)))
   ([type n f]
-    (dotimes [_ n]
-      (future (worker/start type f))))
-  ([type n f sleep]
-    (dotimes [_ n]
-      (future (worker/start type f sleep)))))
+    (repeatedly n
+      #(.start (Worker (name type) f)))))
--- src/clj_kue/worker.clj
+++ src/clj_kue/worker.clj
@@ -1,9 +1,10 @@
 (ns clj-kue.worker
+  (:refer-clojure :exclude [get set])
   (:require [clj-kue.redis  :as r])
-  (:use [clj-kue.job :only [getJob]])
-  (:use clj-kue.util))
+  (:use (clj-kue  [job :only [getJob]]
+                  util)))
 
-(defn get-next-job
+(defn- get-next-job
   "Attempt to fetch the next job"
   [type]
   (let [lkey  (str "q:" type ":jobs")
@@ -17,7 +18,7 @@
         last
         ffirst)))
 
-(defn process [job f]
+(defn- process [job f]
   (try  (let [start (. System (nanoTime))]
           (f job)
           (-> (. System (nanoTime))
@@ -29,14 +30,45 @@
               (.error job e)
               false))))
 
-(defn start
-  "Start processing jobs with the given function f"
-  ([type f]
-    (start type f 1000))
-  ([type f sleep]
+(defprotocol IKueWorker
+  (get [this k]
+    "Get the value of a property")
+  (set [this k v]
+    "Set new value of a property")
+  (getset [this k v]
+    "Set new value of a property and return the old one")
+  (step [this]
+    "Try to process single job")
+  (start [this]
+    "Start processing jobs")
+  (stop [this] [this timeout]
+    "Stop processing jobs"))
+
+(deftype KueWorker [state]
+
+  IKueWorker
+
+  (get [this k]
+    (clojure.core/get @state (keyword k)))
+
+  (set [this k v]
+    (dosync
+      (alter state assoc (keyword k) v))
+    this)
+
+  (getset [this k v]
+    (dosync
+      (let [k*  (keyword k)
+            old (clojure.core/get (ensure state) k*)]
+        (when-not (= old v)
+          (alter state assoc k* v))
+        old)))
+
+  (step [this]
     (with-log-err
-      (when-let [id (get-next-job type)]
-        (let [job (getJob id)]
+      (when-let [id (get-next-job (.get this :type))]
+        (let [job (getJob id)
+              f   (.get this :handler)]
           (.active job)
           (if-let [t  (process job f)]
             (do (.complete job)
@@ -46,5 +78,32 @@
             (if (.attempt job)
                 (.inactive job)
                 (.failed job))))))
-    (Thread/sleep sleep)
-    (recur type f sleep)))
+    this)
+
+  (start [this]
+    (when-not (.getset this :active true)
+      (.set this :future
+        (future
+          (loop []
+            (.step this)
+            (when (.get this :active)
+              (recur))))))
+    this)
+
+  (stop [this]
+    (.stop this 0))
+
+  (stop [this timeout]
+    (when-let [f  (.get this :future)]
+      (let [f*  (future
+                  (Thread/sleep timeout)
+                  (future-cancel f))]
+        (when (.getset this :active false)
+          (safely @(.get this :future))
+          (.set this :future nil))
+        (future-cancel f*)))
+    this))
+
+(defn Worker [type f]
+  (new KueWorker (ref { :handler  f
+                        :type     type})))
